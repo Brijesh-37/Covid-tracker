e518887beba8e18ea84d4b01c99a09ac278e6f1e
Brijesh-37 <brijeshdudhbhate@gmail.com>
2022-11-12 12:34:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [data,setData] = useState([])
+
+  useEffect(() => {
+    fetch("https://data.covid19india.org/data.json")
+      .then((res) => res.json())
+      .then((jsondata) => setData(jsondata.statewise));
+  },[]);
+
+  // const tracker = "https://data.covid19india.org/data.json";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <center>
+      <h3>Covid Tracker</h3>
+      <table className="table table-hover" style={{"width":"60%"}}>
+        <thead>
+          <tr>
+            <th>State</th>
+            <th className="table-warning">Confirmed</th>
+            <th className="table-primary">Recovered</th>
+            <th style={{"backgroundColor":"#ff6666","color":"white"}}>Deaths</th>
+            <th className="table-danger">Active</th>
+            <th>Last Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item=>{
+            return(
+              <tr>
+                <td>{item.state}</td>
+                <td className="table-warning">{item.confirmed}</td>
+                <td className="table-primary">{item.recovered}</td>
+                <td style={{"backgroundColor":"red","color":"white"}}>{item.deaths}</td>
+                <td className="table-danger">{item.active}</td>
+                <td>{item.lastupdatedtime}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      </center>
     </div>
   );
 }
